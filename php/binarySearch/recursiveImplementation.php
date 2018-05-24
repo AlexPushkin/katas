e87c0cef5a1347dc1745d8d8548d875b0f1321f7
Aleksei Pushka <aleksey.pushkin@gen.tech>
2018-05-24 18:22:06 +0300

    working

--- php/binarySearch/recursiveImplementation.php
+++ php/binarySearch/recursiveImplementation.php
@@ -1,55 +1,65 @@
 <?php
 declare(strict_types=1);
 
-function binarySearch(int $needle, array $items): int
+function binarySearch(int $needle, array $items, int $start, int $end): int
 {
-    $count = \count($items);
+    $count = $end - $start;
 
     if ($count === 0) {
         return -1;
     }
 
     if ($count === 1) {
-        return $needle === $items[0] ? 0 : -1;
+        return $needle === $items[$start] ? $start : -1;
     }
 
-    $high = $count - 1;
-    $mid = (int)floor($high / 2);
+    $mid = (int)floor($start + $end / 2);
     $midItem = $items[$mid];
 
     if ($midItem === $needle) {
         return $mid;
     }
 
-    if ($high === 1) {
-        return $needle === $items[$high] ? $high : -1;
-    }
+//    if ($end - $mid === 1) {
+//        return $needle === $items[$end] ? $end : -1;
+//    }
 
     if ($mid < $needle) {
-        return binarySearch($needle, array_slice($items, $mid));
+        return binarySearch($needle, $items, $mid, $end);
     }
 
-    return binarySearch($needle, array_slice($items, 0, $mid));
+    return binarySearch($needle, $items, $start, $mid);
 }
 
-assert(-1 === binarySearch(3, []));
-assert(-1 === binarySearch(3, [1]));
-assert(0 === binarySearch(1, [1]));
+function binaryWrapper(int $needle, array $items): int
+{
+    $count = \count($items);
+    if ($count === 1) {
+        return $needle === $items[0] ? 0 : -1;
+    }
+
+    return binarySearch($needle, $items, 0, $count - 1);
+}
+
+
+assert(-1 === binaryWrapper(3, []));
+assert(-1 === binaryWrapper(3, [1]));
+assert(0 === binaryWrapper(1, [1]));
 #
-assert(0 === binarySearch(1, [1, 3, 5]));
-assert(1 === binarySearch(3, [1, 3, 5]));
-assert(2 === binarySearch(5, [1, 3, 5]));
-assert(-1 === binarySearch(0, [1, 3, 5]));
-assert(-1 === binarySearch(2, [1, 3, 5]));
-assert(-1 === binarySearch(4, [1, 3, 5]));
-assert(-1 === binarySearch(6, [1, 3, 5]));
+assert(0 === binaryWrapper(1, [1, 3, 5]));
+assert(1 === binaryWrapper(3, [1, 3, 5]));
+assert(2 === binaryWrapper(5, [1, 3, 5]));
+assert(-1 === binaryWrapper(0, [1, 3, 5]));
+assert(-1 === binaryWrapper(2, [1, 3, 5]));
+assert(-1 === binaryWrapper(4, [1, 3, 5]));
+assert(-1 === binaryWrapper(6, [1, 3, 5]));
 #
-assert(0 === binarySearch(1, [1, 3, 5, 7]));
-assert(1 === binarySearch(3, [1, 3, 5, 7]));
-assert(2 === binarySearch(5, [1, 3, 5, 7]));
-assert(3 === binarySearch(7, [1, 3, 5, 7]));
-assert(-1 === binarySearch(0, [1, 3, 5, 7]));
-assert(-1 === binarySearch(2, [1, 3, 5, 7]));
-assert(-1 === binarySearch(4, [1, 3, 5, 7]));
-assert(-1 === binarySearch(6, [1, 3, 5, 7]));
-assert(-1 === binarySearch(8, [1, 3, 5, 7]));
+assert(0 === binaryWrapper(1, [1, 3, 5, 7]));
+assert(1 === binaryWrapper(3, [1, 3, 5, 7]));
+assert(2 === binaryWrapper(5, [1, 3, 5, 7]));
+assert(3 === binaryWrapper(7, [1, 3, 5, 7]));
+assert(-1 === binaryWrapper(0, [1, 3, 5, 7]));
+assert(-1 === binaryWrapper(2, [1, 3, 5, 7]));
+assert(-1 === binaryWrapper(4, [1, 3, 5, 7]));
+assert(-1 === binaryWrapper(6, [1, 3, 5, 7]));
+assert(-1 === binaryWrapper(8, [1, 3, 5, 7]));
